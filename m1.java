fb514759680f1a52f31ac9f5b584381917aa3cb5
maheshnc2004 <110278357+maheshnc2004@users.noreply.github.com>
2025-03-21 15:13:43 +0530

    Update m1.java

--- m1.java
+++ m1.java
@@ -1,13 +1,6 @@
-public class AddTwoNumbers {
+public class m1{
     public static void main(String[] args) {
-        // Define two numbers statically
-        double num1 = 5.0;
-        double num2 = 7.0;
-
-        // Add the two numbers
-        double sum = num1 + num2;
-
-        // Display the result
-        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+       
+        System.out.println("hi hello");
     }
 }
